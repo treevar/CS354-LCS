026f2ac7b3e248275e21296b78298e389792c85c
trevor <github@lfod.top>
2025-10-15 22:13:16 -0500

    Print strings

--- compare.sh
+++ compare.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 stringA="AGGTAB"
 stringB="GXTXAYB"
+echo "String A: $stringA"
+echo "String B: $stringB"
 echo "Running Non-Space Optimized LCS..."
 echo
 { echo "$stringA $stringB"; } | ./bin/lcs
