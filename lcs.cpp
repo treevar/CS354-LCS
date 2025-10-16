e1f17a78150ab99cbd11eb136462ba3a73d2ec12
trevor <github@lfod.top>
2025-10-16 18:26:16 -0500

    Fix order

--- lcs.cpp
+++ lcs.cpp
@@ -3,18 +3,18 @@
 #include <cstring>
 #include "Timer.h"
 
-int lcs(const std::string &b, const std::string &a){
-    const int TABLE_SIZE = (a.length()+1) * (b.length()+1);
+int lcs(const std::string &a, const std::string &b){
+    const int TABLE_SIZE = (b.length()+1) * (a.length()+1);
     int *table = new int[TABLE_SIZE];
     memset(table, 0, TABLE_SIZE * sizeof(typeof(table[0]))); //0 array
 
     auto tableAt = [&](int col, int row){
-        return table + (col + row * b.length());
+        return table + (col + row * a.length());
     };
 
-    for(int i = 1; i <= b.length(); ++i){
-        for(int j = 1; j <= a.length(); ++j){
-            if(a[j-1] == b[i-1]){
+    for(int i = 1; i <= a.length(); ++i){
+        for(int j = 1; j <= b.length(); ++j){
+            if(b[j-1] == a[i-1]){
                 *tableAt(i, j) = *tableAt(i-1, j-1) + 1;
             }
             else{
@@ -22,7 +22,7 @@ int lcs(const std::string &b, const std::string &a){
             }
         }
     }
-    int value = *tableAt(b.length(), a.length());
+    int value = *tableAt(a.length(), b.length());
     delete[] table;
     return value;
 }
